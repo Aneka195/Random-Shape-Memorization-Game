9c2708fdc83f9ed7eecf57a60d9b151623d3f9dc
paradox76 <prabh76@my.yorku.ca>
2023-04-18 23:38:39 -0400

    Final html from P

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-/*function highlight(shape){
+function highlight(shape){
     $("#"+shape).css("border", "solid lightblue");
 }
 function unlight(shape){
@@ -6,26 +6,37 @@ function unlight(shape){
     $("#"+shape).css("border", "hidden");  
 
 }
-Uncomment this later on
-*/
+function infoon(){
+    $("#info").removeClass("hidden").addClass("overlay");
+}
+function infooff(){
+    $("#info").addClass("hidden").removeClass("overlay");
+}
+//Uncomment this later on
+
     var inarr=[];   //array of user input on imgs
     var outarr=[]; //array of outputs random imgs
+    var midarr=[];
 function game(){ 
     
 
     var flag=true;
     var i=0;
     var incnt=0;
+    var l=0;
    randcnt(i);
+   $("form img").click(function(){ 
+    incnt = $(this).attr("alt");
+     midarr.push(incnt);})
 
     $("form img").click(function(){ 
         incnt = $(this).attr("alt");
          inarr.push(incnt);
-
+       
     
     if(inarr.length==outarr.length){
 
-        for(var l=0;l<outarr.length && flag==true;l++){
+        for( l=0;l<outarr.length && flag==true;l++){
             if(inarr[l]==outarr[l]){
                 
                 flag=true;
@@ -37,14 +48,17 @@ function game(){
         i++;
         if(flag == true){
             randcnt(i);
+            inarr.length=0;
         }
     }
+    
     });
 
     }
 function randcnt(a){
     for(var k=0;k<outarr.length;k++){
-        $("img[alt='"+outarr[k]+"'").css("border","solid red"); 
+       // $("img[alt='"+outarr[k]+"'").css("border","solid red"); 
+        preimgvisi(k,outarr);
     }
     var j=a;
     do {
@@ -58,31 +72,52 @@ function randcnt(a){
 
 
 }
-function preimgvisi(){
+function preimgvisi(b,lenth){
+    if(lenth[b]==0)
+    {$("img[alt='"+lenth[b]+"'").hide(00001,function(){
+     $("img[alt='"+lenth[b]+"'").attr("src","images/triangle fill.svg").show().animate({opacity:'0'},(500),function(){
+                     $("img[alt='"+lenth[b]+"'").attr("src","images/triangle.svg").animate({opacity:'1'});});
+    });}
 
+    if(lenth[b]==1)
+    {$("img[alt='"+lenth[b]+"'").hide(00001,function(){
+     $("img[alt='"+lenth[b]+"'").attr("src","images/circlefill.svg").show().animate({opacity:'0'},(500),function(){
+                     $("img[alt='"+lenth[b]+"'").attr("src","images/circle.svg").animate({opacity:'1'});});
+    });}
 
+    if(lenth[b]==2)
+    {$("img[alt='"+lenth[b]+"'").hide(00001,function(){
+     $("img[alt='"+lenth[b]+"'").attr("src","images/starfill.svg").show().animate({opacity:'0'},(500),function(){
+                     $("img[alt='"+lenth[b]+"'").attr("src","images/star.svg").animate({opacity:'1'});});
+    });}
+
+    if(lenth[b]==3)
+    {$("img[alt='"+lenth[b]+"'").hide(00001,function(){
+     $("img[alt='"+lenth[b]+"'").attr("src","images/squarefill.svg").show().animate({opacity:'0'},(500),function(){
+                     $("img[alt='"+lenth[b]+"'").attr("src","images/square.svg").animate({opacity:'1'});});
+    });}
 }
 
 
 function imgvisi(r){
   if(r==0)
    {$("img[alt='"+r+"'").hide(00001,function(){
-    $("img[alt='"+r+"'").attr("src","images/triangle fill.svg").show().animate({opacity:'0'},2000,function(){
+    $("img[alt='"+r+"'").attr("src","images/triangle fill.svg").show().animate({opacity:'0'},3000,function(){
                     $("img[alt='"+r+"'").attr("src","images/triangle.svg").animate({opacity:'1'});});
    });}
    if(r==1)
    {$("img[alt='"+r+"'").hide(00001,function(){
-    $("img[alt='"+r+"'").attr("src","images/circlefill.svg").show().animate({opacity:'0'},2000,function(){
+    $("img[alt='"+r+"'").attr("src","images/circlefill.svg").show().animate({opacity:'0'},3000,function(){
                     $("img[alt='"+r+"'").attr("src","images/circle.svg").animate({opacity:'1'});});
    });}
    if(r==2)
    {$("img[alt='"+r+"'").hide(00001,function(){
-    $("img[alt='"+r+"'").attr("src","images/starfill.svg").show().animate({opacity:'0'},2000,function(){
+    $("img[alt='"+r+"'").attr("src","images/starfill.svg").show().animate({opacity:'0'},3000,function(){
                     $("img[alt='"+r+"'").attr("src","images/star.svg").animate({opacity:'1'});});
    });}
    if(r==3)
    {$("img[alt='"+r+"'").hide(00001,function(){
-    $("img[alt='"+r+"'").attr("src","images/squarefill.svg").show().animate({opacity:'0'},2000,function(){
+    $("img[alt='"+r+"'").attr("src","images/squarefill.svg").show().animate({opacity:'0'},3000,function(){
                     $("img[alt='"+r+"'").attr("src","images/square.svg").animate({opacity:'1'});});
    });}
 
